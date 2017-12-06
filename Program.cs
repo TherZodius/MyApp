9395c09606f63c10c21a533682dd6d0dadf664d6
TherZodius <josealejandro@outlook.fr>
2017-12-06 16:23:07 +0100

    esa
    esa

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace MyApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+        async static void update()
+        {
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://therzodiusinc.000webhostapp.com/Releases/"))
+            {
+                await mgr.Result.UpdateApp();
+            }
+        }
+
+    }
+}
